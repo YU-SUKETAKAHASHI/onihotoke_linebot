45713353da36611a204205dc027a8c01ea6f5f37
yusuke takahashi <thys_60918@yahoo.co.jp>
2020-04-14 18:51:23 +0900

    DBurlかくした

--- database/setup_db.py
+++ database/setup_db.py
@@ -2,13 +2,27 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 
-db_1 = pd.read_csv("kamoku_r02_0310.csv", encoding="cp932")
-db_2 = pd.read_csv("kamoku_r02_0311.csv", encoding="cp932")
-db_3 = pd.read_csv("kamoku_r02_0312.csv", encoding="cp932")
+def gen_lecture_info_db():
+    db_1 = pd.read_csv("kamoku_r02_0310.csv", encoding="cp932")
+    db_2 = pd.read_csv("kamoku_r02_0311.csv", encoding="cp932")
+    db_3 = pd.read_csv("kamoku_r02_0312.csv", encoding="cp932")
 
-engine = create_engine("postgres://iyclxsbznyhojl:1f5ec245d0bbf4bf5e58204da1ef0b172f971c102efe38fad8a1e36699a19974@ec2-18-210-51-239.compute-1.amazonaws.com:5432/de9fvgtnehbuju")
-db_1.to_sql("lecture_info", engine, if_exists="append", index=False)
-db_2.to_sql("lecture_info", engine, if_exists="append", index=False)
-db_3.to_sql("lecture_info", engine, if_exists="append", index=False)
-print("finish!")
+    engine = create_engine(DATABASEURL)
+    db_1.to_sql("lecture_info", engine, if_exists="append", index=False)
+    db_2.to_sql("lecture_info", engine, if_exists="append", index=False)
+    db_3.to_sql("lecture_info", engine, if_exists="append", index=False)
+    print("finish!")
+
+def gen_user_info_db():
+    data = {"department": ["学部"],
+            "subject": ["学科"],
+            "userid": ["1234567890"]}
+    userid_df = pd.DataFrame(data)
+
+    engine = create_engine(DATABASEURL)
+    userid_df.to_sql("user_info", engine, if_exists="append", index=False)
+
+if __name__ == "__main__":
+    #gen_lecture_info_db()
+    gen_user_info_db()
 
