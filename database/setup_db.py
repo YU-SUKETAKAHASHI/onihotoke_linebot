f13a6dbe38123a139217b6b5f5e2817f208bca44
yusuke takahashi <thys_60918@yahoo.co.jp>
2020-04-15 13:33:26 +0900

    やっちゃうよ

--- database/setup_db.py
+++ database/setup_db.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import os
 
 
 def gen_lecture_info_db():
@@ -18,20 +19,19 @@ def gen_lecture_info_db():
 
 
 def gen_user_info_db():
-    DATABASE_URL = os.environ["DATABASE_URL"]
-    data = {"department": ["学部"],
-            "subject": ["学科"],
+    # DATABASE_URL = os.environ["DATABASE_URL"]
+    data = {"major": ["学部"],
             "userid": ["1234567890"]}
     userid_df = pd.DataFrame(data)
     try:
-        engine = create_engine(DATABASE_URL)
-        userid_df.to_sql("user_info", engine, if_exists="append", index=False)
+        engine = create_engine("postgres://iyclxsbznyhojl:1f5ec245d0bbf4bf5e58204da1ef0b172f971c102efe38fad8a1e36699a19974@ec2-18-210-51-239.compute-1.amazonaws.com:5432/de9fvgtnehbuju")
+        userid_df.to_sql("user_info", engine, if_exists="replace", index=False)
         print("success")
     except:
         print("faild")
 
 
 if __name__ == "__main__":
-    gen_lecture_info_db()
+    #gen_lecture_info_db()
     gen_user_info_db()
 
