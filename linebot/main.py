fdf97b3f697f63d514ffe1c35dc9b38d6afe9771
31159Ko-suke <k426.31159@gmail.com>
2020-04-16 03:38:01 +0900

    修正

--- linebot/main.py
+++ linebot/main.py
@@ -137,7 +137,7 @@ def on_postback(event):
         lecture_info = search_lecture_info(lecture_group, user_major) # 講義情報の辞書のリストが返ってくる
         print(lecture_info)
         print(user_major=="工" and post_data=="自然科学")
-        if (user_major=="機知" or user_major=="情物"or user_major=="化バイ" or user_major=="材料" or user_major=="建築"　or user_major=="理") and post_data=="自然科学":
+        if (user_major=="機知" or user_major=="情物" or user_major=="化バイ" or user_major=="材料" or user_major=="建築" or user_major=="理") and post_data=="自然科学":
              line_bot_api.reply_message(
                     event.reply_token,
                     TextSendMessage(text="件数が多いため表示できません"))
