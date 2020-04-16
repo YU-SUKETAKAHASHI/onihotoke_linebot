8cfa2d87612f777c6f36d0de9285deaba4cf9677
31159Ko-suke <k426.31159@gmail.com>
2020-04-16 15:22:56 +0900

    最新

--- linebot/main.py
+++ linebot/main.py
@@ -302,8 +302,8 @@ def handle_message(event):
         kibutsuList = searchAll(texts[0], texts[1].split("，")[0])#講義情報の辞書のリスト
         print(kibutsuList)
         if kibutsuList :
-            if len(kibutsuList)>10:
-                kibutsuList = kibutsuList[:10]
+            # if len(kibutsuList)>10:
+            kibutsuList = kibutsuList[:10]
             try:
                 line_bot_api.reply_message(
                     event.reply_token,
