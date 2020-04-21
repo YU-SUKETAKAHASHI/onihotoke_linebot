95e82e6504cb9ebe94c35c6a5cd12a0f70979796
yusuke takahashi <thys_60918@yahoo.co.jp>
2020-04-21 22:26:09 +0900

    DBURLかくし

--- database/setup_u_sy_db.py
+++ database/setup_u_sy_db.py
@@ -4,12 +4,12 @@ import os
 
 
 def gen_lecture_info_db():
-    # DATABASE_URL = os.environ["DATABASE_URL"]
+    DATABASE_URL = os.environ["DATABASE_URL"]
     db_1 = pd.read_csv("kamoku_r02_0310.csv", encoding="cp932")
     db_2 = pd.read_csv("kamoku_r02_0311.csv", encoding="cp932")
     db_3 = pd.read_csv("kamoku_r02_0312.csv", encoding="cp932")
     try:
-        engine = create_engine("postgres://iyclxsbznyhojl:1f5ec245d0bbf4bf5e58204da1ef0b172f971c102efe38fad8a1e36699a19974@ec2-18-210-51-239.compute-1.amazonaws.com:5432/de9fvgtnehbuju")
+        engine = create_engine(DATABASE_URL)
         db_1.to_sql("lecture_info", engine, if_exists="replace", index=False)
         db_2.to_sql("lecture_info", engine, if_exists="append", index=False)
         db_3.to_sql("lecture_info", engine, if_exists="append", index=False)
